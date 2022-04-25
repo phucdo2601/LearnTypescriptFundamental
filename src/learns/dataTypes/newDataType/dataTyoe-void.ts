e86a9753232956597cb957afdac047a8a3f10430
phucdo2601 <ngocphucdo2601@gmail.com>
2022-04-25 23:07:03 +0700

    First learn b ep kieu trong ts

--- src/learns/dataTypes/newDataType/dataTyoe-void.ts
+++ src/learns/dataTypes/newDataType/dataTyoe-void.ts
@@ -10,3 +10,21 @@ function demoFunction(): void {
 }
 
 demoFunction();
+
+//ep kieu trong ts
+
+let kieu1 : any;
+kieu1 = "Day la  vi du ve ep kieu trong ts";
+// console.log((<string> kieu1).length);
+console.log((kieu1 as string).length);
+
+let xxx : string;
+xxx = "140";
+
+/**
+ * Ep Kieu chi tuong thich voi kieu any, khong tuong thich voi cac kieu du lieu cu the
+ */
+// console.log((xxx as number) + 60);
+
+
+
