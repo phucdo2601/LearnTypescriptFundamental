7ccfd5ace91c92bb14ef3b3ec65a74b32825fa08
phucdo2601 <ngocphucdo2601@gmail.com>
2022-04-25 22:22:23 +0700

    First learn b  boolean, enum, Tuple in ts

---
+++ src/learns/dataTypes/newDataType/dataType-boolean.ts
@@ -0,0 +1,3 @@
+//boolean: kieu dung sai
+var flag : boolean = false;
+console.log(`Gia tri mac dinh cua kien boolean: ${flag}`);
---
+++ src/learns/dataTypes/newDataType/enum.ts
@@ -0,0 +1,10 @@
+/**
+ * Kieu enum la no gan trang thai cho mot phan tu
+ * Khai bao enum trong ts: enum ten-bien{};
+ */
+
+enum workingStatus {
+    "Free" = 10, "Working", "Accept-Order"
+};
+
+console.log(workingStatus.Free);
