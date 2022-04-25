40cbb02b17f49905a68782b61b77024c412a91fd
phucdo2601 <ngocphucdo2601@gmail.com>
2022-04-25 22:54:53 +0700

    First learn b dataType void in ts

---
+++ src/learns/dataTypes/newDataType/dataTyoe-void.ts
@@ -0,0 +1,12 @@
+//kieu du lieu void
+/**
+ * Kieu void thi khong dc return,
+ * thuc hien cau lenh nham in ra hoac ko can thiet tra ve gia tri
+ * @param params 
+ */
+function demoFunction(): void {
+    console.log("Hello World!!!");
+    // return 100;
+}
+
+demoFunction();
