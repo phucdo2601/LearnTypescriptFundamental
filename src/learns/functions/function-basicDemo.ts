01fa5b504311ee43c20081343887ef9ae29f130b
phucdo2601 <ngocphucdo2601@gmail.com>
2022-05-03 08:20:55 +0700

    First learn basic function in ts b01

---
+++ src/learns/functions/function-basicDemo.ts
@@ -0,0 +1,56 @@
+/**
+ * Viet demo mot ham tra ve kieu number
+ */
+
+function tinhTong() : number {
+    
+    return 100 + 200;
+}
+
+console.log(tinhTong());
+
+/**
+ * ham tra ve string
+ */
+
+function kieuString() : string {
+    return "Chao Buoi sang";
+}
+
+console.log(kieuString());
+
+/**
+ * Ham tra ve kieu mang
+ */
+function kieuMang() : string[] {
+    return ["phucdn", "vinhLq", "tramHtk"];
+}
+
+console.log(kieuMang());
+
+/**
+ * Ham tra ve mang number
+ */
+
+function mangSo() : number[] {
+    return [4,5,6,7,8];
+}
+
+/**
+ * Mang tra ve mot doi tuong (any)
+ */
+function kieuDoiTuong() : any {
+    //khai bao mot bien co kieu du lieu la object de luu tru mot gia tri doi tuong
+    var userInfo : object;
+    userInfo = {
+        name: "phucdn",
+        hobbies: {
+            hob1: "reading a book",
+            hob2: "listening music"
+        }
+    }
+
+    return userInfo;
+}
+
+console.log(kieuDoiTuong());
