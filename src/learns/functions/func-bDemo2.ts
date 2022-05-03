9d1c1ee7f9074d128c6e1b9ca481161551eefc67
phucdo2601 <ngocphucdo2601@gmail.com>
2022-05-03 08:47:42 +0700

    Learn tai sao su dung ham va ham co tham so dau vao b01

---
+++ src/learns/functions/func-bDemo2.ts
@@ -0,0 +1,45 @@
+// var x_func_b_01 : number;
+// var y_func_b_01 : number;
+// var tbc_func_b_01 : number;
+
+// x_func_b_01 = 10;
+// y_func_b_01 =15;
+
+// tbc_func_b_01 =(x_func_b_01 + y_func_b_01) /2;
+// console.log(`trung binh cong cua ${x_func_b_01} va ${x_func_b_01} la: ${tbc_func_b_01}`);
+
+// x_func_b_01= 1000;
+// y_func_b_01 = 722;
+// tbc_func_b_01 =(x_func_b_01 + y_func_b_01) /2;
+// console.log(`trung binh cong cua ${x_func_b_01} va ${x_func_b_01} la: ${tbc_func_b_01}`);
+
+/**
+ * Ham Khong tham so dau vao
+ */
+// function tinh1() : void {
+//     x_func_b_01 = 10;
+//     y_func_b_01 =15;
+//     tbc_func_b_01 =(x_func_b_01 + y_func_b_01) /2;
+//     console.log(`trung binh cong cua ${x_func_b_01} va ${y_func_b_01} la: ${tbc_func_b_01}`);
+// }
+
+/**
+ * Ham chua tham so dau vao
+ */
+function tinhTbc(x:number, y:number) : void {
+    var tbc =(x + y) /2;
+    console.log(`trung binh cong cua ${x} va ${y} la: ${tbc}`);
+}
+
+/**
+ * tinh ham khong tham so truyen vao
+ */
+// tinh1();
+
+/**
+ * tinh ham co tham so truyen vao
+ */
+tinhTbc(10,20);
+tinhTbc(100,99);
+tinhTbc(668,37);
+
