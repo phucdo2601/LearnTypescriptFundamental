57a40f8d32b8d3709182ad51965062ff725c9fbd
phucdo2601 <ngocphucdo2601@gmail.com>
2022-05-03 13:45:28 +0700

    Learn static with class in ts b01

--- src/learns/learn_oop/fundamental/congViec.ts
+++ src/learns/learn_oop/fundamental/congViec.ts
@@ -6,20 +6,34 @@ enum state {
 
 class congViec {
 
-    id: number;
-    name: string;
+    static id: number = 2;
+    static nameWork: string = "viec so 1";
     status: state;
 
     constructor(id: number, name: string, status: state) {
-        this.id = id;
-        this.name = name;
+        congViec.id = id;
+        congViec.nameWork = name;
         this.status = status;
     }
 
     toString(): string {
-        return `${this.id} -ten: ${this.name} -status: ${this.status}`;
+        return `${congViec.id} -ten: ${congViec.nameWork} -status: ${this.status}`;
+    }
+
+    static testStatic() : string {
+        return  "thu xem co su dung dc ham nay ma ko can phai tai moi mot instance ko?";
+        
     }
 }
 
-let cv1 = new congViec(1, "Hoc Html", state.Finish);
-console.log(cv1.toString());
+//tao mot doi tuong trong class (instance)
+// let cv1 = new congViec(1, "Hoc Html", state.Finish);
+// console.log(cv1.toString());
+
+
+/**
+ * static se giup chung la se khong tao ra mot instance ma van co the su dung dc cac thuoc tinh
+ */
+
+console.log(congViec.nameWork);
+console.log(congViec.testStatic());
